2c966edac63931f749ddfb3a17c9a559c6b8f0ec
Luca <dumitrasculuca07@gmail.com>
2024-01-05 07:42:51 +0200

    Added new tests - Tested method is convertValue() from every converter class

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,14 +1,10 @@
 import Controller.ProgramController;
-import Converter.*;
 
-import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
 
         ProgramController programController = new ProgramController();
         programController.run();
-
-        // Todo : Unit tests
     }
 }
